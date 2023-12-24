ebe624758c8d2162d4d5a64f344ca0412f768d12
Nidansingh202 <107486940+Nidansingh202@users.noreply.github.com>
2023-12-24 12:18:42 +0530

    Add files via upload

---
+++ arrays/array1.java
@@ -0,0 +1,9 @@
+public class array1 {
+    public static void main(String[] args) {
+        String[] name = { "vishal", "Shivam", "ankur", "Sanjay" };
+        System.out.println(name[0]);
+        System.out.println(name[1]);
+        System.out.println(name[2]);
+        System.out.println(name[3]);
+}
+}
---
+++ arrays/array2.java
@@ -0,0 +1,9 @@
+public class array2 {
+    public static void main(String[] args) {
+        String[] name = {"apply","banana","carrot","mango"}
+        arrayList<String> list = new arrayList<String>() ;
+ list.add( "apple" );
+ list.add( "banana" );
+ list.add( "carrot" );
+list.add( 0, "mango" )
+}
