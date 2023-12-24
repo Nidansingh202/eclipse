37ff2e78fcc739d6151621a3d096b39809640031
Nidansingh202 <107486940+Nidansingh202@users.noreply.github.com>
2023-12-24 12:13:41 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,30 @@
+public class Loops {
+ //for loop   
+    public static  void main(String args[]){
+        //for(int counter = 0; counter < 100 ; counter = counter + 1){
+            //for (int counter = 0; counter < 11 ; counter ++) {
+            //System.out.println(" Hello world");
+           // System.out.print(counter+"  ");
+        //}
+         //for(int i= 0; i<11 ; i++){
+           // System.out.print(i);
+         //}
+
+//while  looop
+            
+          //  int i =0;
+         //   while(i <11 ){
+         //       System.out.println(i);
+         //       i = i+ 1; 
+         //   }
+         int i=12;
+         while(i<11){
+            System.out.println("Appa college");
+            
+         }
+         do {
+            System.out.println("Appna college");
+         }while(i<11);
+        }
+    }
+
